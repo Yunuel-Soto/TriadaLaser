dc23f12cf0f12b21f57d0b44c599167384853026
Sistemas-qro-veo <sistemas.qro@corporativoveo.com.mx>
2024-10-07 11:47:55 -0600

    new features pop chat and pop up

--- assets/js/navBarAction.js
+++ assets/js/navBarAction.js
@@ -1,6 +1,8 @@
 var buttonNav = document.getElementById("btn_nav");
-
 var navBar = document.getElementById("nav1");
+var navMenu = document.getElementById("navMenu");
+var navMain = document.getElementById("navMain");
+var upPop = document.getElementById("up");
 
 var openNav = false;
 
@@ -8,8 +10,59 @@ buttonNav.addEventListener("click", function (e) {
   if (openNav) {
     openNav = false;
     navBar.style.height = "0px";
+    navMenu.src ="./assets/img/list.png";
+    
   } else {
     openNav = true;
     navBar.style.height = "200px";
+    navMenu.src ="./assets/img/x.png";
   }
+});
+
+let lastScrollTop = 0;
+
+window.addEventListener('scroll', function() {
+  let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+  console.log(currentScroll);
+
+  if(currentScroll == 0) {
+    upPop.style.display = "none";
+  } else if(currentScroll > 0) {
+    upPop.style.display = "flex";
+  }
+
+  if(currentScroll < lastScrollTop) {
+    navMain.style.position = 'sticky';
+    navMain.style.top = '0px';
+    navMain.style.zIndex = '1000';
+  } else {
+    navMain.style.top = '-200px';
+    navMain.style.position = 'static';
+    navMain.style.zIndex = '1000';
+  }
+
+  lastScrollTop = currentScroll;
+
+});
+
+let lastY = 0;
+
+window.addEventListener('mousemove', function(e) {
+  let currentY = e.clientY;
+
+  if(currentY < lastY) {
+    navMain.style.position = 'sticky';
+    navMain.style.top = '0px';
+    navMain.style.zIndex = '1000';
+  }
+
+  lastY = currentY;
+});
+
+upPop.addEventListener("click", function() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  })
 });
